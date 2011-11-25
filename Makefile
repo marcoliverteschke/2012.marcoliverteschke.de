a13e2e8ddfce6336936859da186ce03338583979
Marc-Oliver Teschke <marcoliverteschke@mac.com>
2011-11-25 14:15:47 +0100

    Design finalized. Added background image. Added IE6 universal stylesheet. Added styles and navigation for lower resolutions. Added minify.

---
+++ Makefile
@@ -0,0 +1,9 @@
+less:
+	lessc htdocs/css/screen.less > htdocs/css/screen.css
+
+
+local: less
+	rsync -aP --delete htdocs/ /Applications/MAMP/htdocs/2012
+
+install: less
+	rsync -aP --delete htdocs/ marcoliverteschke.de@marcoliverteschke.de:htdocs
--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -1,7 +1,34 @@
 $(document).ready(function(){
+	
+	$('#mailto').click(function(){
+		_gaq.push(['_trackPageview', '/mailto']);
+	});
+	
 	$('[role="navigation"] a, a.goto').click(function(e){
 		var anchor = $(this).attr('href').substr(1);
-		$('body').animate({scrollTop: $('[name=' + anchor + ']').offset().top}, 1000);
-		return false;
+		_gaq.push(['_trackPageview', '/' + anchor]);
+		if($('html').hasClass('no-touch'))
+		{
+			$('html,body').animate({scrollTop: $('[name=' + anchor + ']').offset().top}, 1000);
+			return false;
+		}
 	});
+	
+	$(window).scroll(function(){
+		adjust_banner();
+	}).resize(function(){
+		adjust_banner();
+	});
+	
 });
+
+
+function adjust_banner()
+{
+	if($('[role="banner"]').css('position') != 'fixed')
+	{
+		$('[role="banner"]').css('top', $(window).scrollTop());
+	} else {
+		$('[role="banner"]').css('top', 0);
+	}
+}
